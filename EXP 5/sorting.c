9ed01288cb1f87860c814bca88d667c586a12bec
Sahil-Kurkure <96710867+Sahil-Kurkure@users.noreply.github.com>
2022-03-12 12:41:06 +0530

    Update sorting.c

--- EXP 5/sorting.c
+++ EXP 5/sorting.c
@@ -1,3 +1,25 @@
+/*The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from the unsorted part and putting it at the beginning. 
+The algorithm maintains two sub-arrays in a given array.
+    1) The sub-array which is already sorted.
+    2) Remaining sub-array which is unsorted.
+Every iteration of selection sort, the minimum element (considering ascending order) from the unsorted sub-array is picked and moved to the sorted sub-array.
+
+Following example explains the above steps:
+arr[] = 64 25 12 22 11
+
+// Find the minimum element in arr[0...4} and place it at beginning
+11 25 12 22 64
+
+// Find the minimum element in arr[1...4] and place it at beginning of arr[1...4]
+11 12 25 22 64
+
+// Find the minimum element in arr[2...4] and place it at beginning of arr[2...4]
+11 12 22 25 64
+
+// Find the minimum element in arr[3...4] and place it at beginning of arr[3...4]
+11 12 22 25 64
+*/
+
 #include <stdio.h>
 
 void sort(int array[],int size);
