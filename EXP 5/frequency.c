640dd8b1e5475ed460be57c6aa97a5c4b451b40c
Sahil-Kurkure <96710867+Sahil-Kurkure@users.noreply.github.com>
2022-03-12 12:43:17 +0530

    Update frequency.c

--- EXP 5/frequency.c
+++ EXP 5/frequency.c
@@ -1,3 +1,10 @@
+/*Write a program in C to calculate frequency count of each number in ARRAY. 
+
+  Suppose input array is  [ 1 2 1 4 5 2 3 1 4 5 6 2 2 3 ]
+  output :  1 is appearing 3 times , 2 is appearing  4 times ,  4 is appearing 2 times….
+  5 is appearing 2 times , 3 is appearing 2 times ,  6 is appearing 1 times 
+*/
+
 #include <stdio.h>
 
 int main()
