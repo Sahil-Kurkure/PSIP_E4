e985d0fbdd16b1962a74b97b4546975dc2c76100
Sahil-Kurkure <96710867+Sahil-Kurkure@users.noreply.github.com>
2022-03-12 12:29:25 +0530

    Update Mobius_old.c

--- EXP 3/Mobius_old.c
+++ EXP 3/Mobius_old.c
@@ -1,3 +1,15 @@
+/*The Mobius function M (N) is defined as
+M (N) = 1 ; if the number of prime factors are even
+M(N)= 0 ; if any prime factor is contained in N more than once
+M(N)= (-1) ; if the number of prime factors are odd
+
+For example
+M (78) = -1 [ 78 = 2 * 3 * 13 ]
+M (34) = 1 [ 34 = 2 * 17 ]
+M (45) = 0 [ 45 = 3 * 3 * 5 ]
+
+Write a function MOBIUS as specified above.*/
+
 #include <stdio.h>
 
 int mobius(int x);
